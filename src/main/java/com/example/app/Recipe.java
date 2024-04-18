0b1099a6e95be4b08c8d2c7d4f9e708ef8ccc929
Leif <leifstoehr333@gmail.com>
2024-04-18 11:53:02 +0200

    Recipe-Klasse hinzugefügt

---
+++ src/main/java/com/example/app/Recipe.java
@@ -0,0 +1,45 @@
+package com.example.app;
+
+    public class Recipe {
+        private String name;
+        private String description;
+        private int prepTime;
+        private int cookingTime;
+        private String ingredients;
+        private int servings;
+
+        public Recipe(String name, String description, int prepTime, int cookingTime, String ingredients, int servings){
+            this.name = name;
+            this.description = description;
+            this. prepTime = prepTime;
+            this.cookingTime = cookingTime;
+            this.ingredients = ingredients;
+            this.servings = servings;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public int getPrepTime() {
+            return prepTime;
+        }
+
+        public int getCookingTime() {
+            return cookingTime;
+        }
+
+        public String getIngredients() {
+            return ingredients;
+        }
+
+        public int getServings() {
+            return servings;
+        }
+    }
+
+}
