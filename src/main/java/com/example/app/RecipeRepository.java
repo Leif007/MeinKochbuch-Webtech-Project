2daad874b41045eb37a89cab16ab8af1ce6186a4
Leif <leifstoehr333@gmail.com>
2024-06-01 18:26:10 +0200

    RecipeService hinzugefügt

--- src/main/java/com/example/app/RecipeRepository.java
+++ src/main/java/com/example/app/RecipeRepository.java
@@ -1,8 +1,8 @@
 package com.example.app;
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long> { }
+public interface RecipeRepository extends CrudRepository<Recipe, Long> { }
---
+++ src/main/java/com/example/app/RecipeService.java
@@ -0,0 +1,23 @@
+package com.example.app;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RecipeService {
+
+    @Autowired
+    RecipeRepository repo;
+
+    public Recipe save(Recipe recipe) {
+        return repo.save(recipe);
+    }
+
+    public Recipe getRecipeById(Long id) {
+        return repo.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+
+}
