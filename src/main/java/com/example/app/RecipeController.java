50b167d3c9262357ed3569c52af17b8e71eb5f0a
KingChaos16 <smpcraft12345@gmail.com>
2024-06-27 13:22:29 +0200

    FatSecretAPI v2

--- src/main/java/com/example/app/RecipeController.java
+++ src/main/java/com/example/app/RecipeController.java
@@ -44,10 +44,7 @@ public class RecipeController {
         return service.getAllRecepies();
     }
 
-    @GetMapping("/recipes")
-    public Iterable<Recipe> getAllRecipes() {
-        return service.getAllRecepies();
-    }
+
 
 
 }
